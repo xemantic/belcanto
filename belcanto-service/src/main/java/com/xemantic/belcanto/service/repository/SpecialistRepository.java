1c448254ee8223d882abc33393251cb9274f8aeb
Kazik Pogoda <morisil@xemantic.com>
2017-06-13 14:07:22 +0200

    repositories updated to the JpaRepository, allows paging

--- belcanto-service/src/main/java/com/xemantic/belcanto/service/repository/SpecialistRepository.java
+++ belcanto-service/src/main/java/com/xemantic/belcanto/service/repository/SpecialistRepository.java
@@ -1,15 +1,15 @@
 package com.xemantic.belcanto.service.repository;
 
-import com.xemantic.belcanto.model.Customer;
+import com.xemantic.belcanto.model.Specialist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
- * Repository of {@link Customer}s.
+ * Repository of {@link Specialist}s.
  *
  * @author morisil
  */
 @RepositoryRestResource
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface SpecialistRepository extends JpaRepository<Specialist, Long> {
   /* instantiated by spring-data-rest via reflection */
 }
